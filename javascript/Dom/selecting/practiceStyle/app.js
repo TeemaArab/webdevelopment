50b4082cb1cae8151950b45425a397f6d3853d94
Fatima <fatima.arab@gmail.com>
2025-03-04 22:16:04 -0500

    stillSelector

---
+++ javascript/Dom/selecting/practiceStyle/app.js
@@ -0,0 +1,5 @@
+const newStyle = document.querySelector('#container');
+newStyle.style.textAlign = 'center';
+const newStyle2 = document.querySelector('img')
+newStyle2.style.width = '150px';
+newStyle2.style.borderRadius = '50%';
